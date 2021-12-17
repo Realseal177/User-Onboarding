a3e3273fffbe66225a4e45e6785c3f82aba6df89
Andrew Nelson <Realseal177@gmail.com>
2021-12-16 20:25:51 -0800

    proper elements showing

--- user-onboarding/cypress/integration/form_test.js
+++ user-onboarding/cypress/integration/form_test.js
@@ -1,5 +1,5 @@
 describe('User Onboarding', () => {
-    beforeEach(() {
+    beforeEach(() => {
         cy.visit('http://localhost:3000')
     })
 
@@ -8,6 +8,26 @@ describe('User Onboarding', () => {
     const eMail = () => cy.get('input[name=email]');
     const passWord = () => cy.get('input[name=password]');
     const submitBtn = () => cy.get('button[id="submitButton"]');
-    const checkBox = () => cy.get('checkbox[name=terms]');
+    const checkBox = () => cy.get('[type="checkbox"]');
+
+    it('sanity check to make sure tests work', () => {
+        // 'it' is a test
+        // expect is an assertion
+        expect(1 + 2).to.equal(3);
+        expect(2 + 2).not.to.equal(5) // strict equality ===
+        expect({}).not.to.equal({}); // strict equality {} !== {}
+        expect({}).to.eql({}); // not strict ==
+    })
+
+    it('proper elements showing', () => {
+        firstName().should('exist');
+        lastName().should('exist');
+        eMail().should('exist');
+        passWord().should('exist');
+        submitBtn().should('exist');
+        checkBox().should('exist');
+    })
+
+    // describe('Filling out inputs and clearing when submitted')
 
 })
